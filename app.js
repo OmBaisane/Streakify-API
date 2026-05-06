3fb85c3e2d3e94e9798ffcba928b101b83b15f5f
Om Baisane <ombaisane29@gmail.com>
2026-05-06 15:28:12 +0530

    Initial project setup (Express + MongoDB)

---
+++ app.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Streakify API Running");
+});
+
+app.listen(3000, () => {
+  console.log("Server running...");
+});
