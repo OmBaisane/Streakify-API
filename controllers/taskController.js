0777d6b5f03b796acd9a2d18ec34f167e38f53ff
Om Baisane <ombaisane29@gmail.com>
2026-05-06 16:54:45 +0530

    Added update & delete task APIs with user security

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -31,3 +31,41 @@ exports.getTasks = async (req, res) => {
     res.status(500).json({ message: "Error" });
   }
 };
+
+exports.updateTask = async (req, res) => {
+  try {
+    const task = await Task.findOneAndUpdate(
+      {
+        _id: req.params.id,
+        user: req.userId,
+      },
+      req.body,
+      { returnDocument: "after" },
+    );
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    res.json(task);
+  } catch (err) {
+    res.status(500).json({ message: "Error" });
+  }
+};
+
+exports.deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findOneAndDelete({
+      _id: req.params.id,
+      user: req.userId,
+    });
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    res.json({ message: "Task deleted" });
+  } catch (err) {
+    res.status(500).json({ message: "Error" });
+  }
+};
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const { createTask, getTasks } = require("../controllers/taskController");
+const {
+  createTask,
+  getTasks,
+  updateTask,
+  deleteTask,
+} = require("../controllers/taskController");
 
 const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/tasks", authMiddleware, createTask);
 router.get("/tasks", authMiddleware, getTasks);
+router.put("/tasks/:id", authMiddleware, updateTask);
+router.delete("/tasks/:id", authMiddleware, deleteTask);
 
 module.exports = router;
